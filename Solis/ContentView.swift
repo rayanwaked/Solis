3bad3058ead8230c132b14c45ce55c916668ab82
Rayan Waked <rayan@waked.dev>
2024-08-21 10:12:05 -0700

    Initial Commit

---
+++ Solis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Solis
+//
+//  Created by Rayan Waked on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Solis/SolisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolisApp.swift
+//  Solis
+//
+//  Created by Rayan Waked on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SolisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
